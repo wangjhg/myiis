849b7427d4af227b657e5da5a5c8c9101434e74f
wangjhg <wangjhg@yahoo.com>
2018-03-28 21:58:29 +0000

    version 0.2.0 add system info

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,7 +4,11 @@ powershell_script 'Install IIS' do
 end
 
 file 'C:\inetpub\wwwroot\Default.htm' do
-   content '<h1>Hello, world!</h1>'
+   content "<h1>Hello, world!</h1>
+<h2>PLATFORM: #{node['platform']}</h2>
+<h2>HOSTNAME: #{node['hostname']}</h2>
+<h2>MEMORY:   #{node['memory']['total']}</h2>
+<h2>CPU Mhz:  #{node['cpu']['0']['mhz']}</h2>"
 end
 
 service 'w3svc' do
