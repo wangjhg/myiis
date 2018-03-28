fff9cc26fed83920b3d4f4483f409ac6c5cc3cb5
wangjhg <wangjhg@yahoo.com>
2018-03-28 22:35:10 +0000

    version 0.2.1 using a template to create a homepage

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,12 +3,8 @@ powershell_script 'Install IIS' do
   action :run
 end
 
-file 'C:\inetpub\wwwroot\Default.htm' do
-   content "<h1>Hello, world!</h1>
-<h2>PLATFORM: #{node['platform']}</h2>
-<h2>HOSTNAME: #{node['hostname']}</h2>
-<h2>MEMORY:   #{node['memory']['total']}</h2>
-<h2>CPU Mhz:  #{node['cpu']['0']['mhz']}</h2>"
+template 'c:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
 end
 
 service 'w3svc' do
